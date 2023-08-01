a4d34a7608fb673dee177be5a40a61e8bbdf8267
AyakaB <a1ya1k2a9@gmail.com>
2023-08-01 07:26:18 -0700

    fixed column data

--- client/src/components/TransactionGrid.jsx
+++ client/src/components/TransactionGrid.jsx
@@ -29,11 +29,11 @@ const TransactionGrid = ({ transactionData }) => {
       >
         <ColumnsDirective>
           <ColumnDirective field="amount" headerText="Amount" width="100" />
-          <ColumnDirective
+          {/* <ColumnDirective
             field="category_id"
             headerText="Category ID"
             width="100"
-          />
+          /> */}
           <ColumnDirective field="currency" headerText="Currency" width="100" />
           <ColumnDirective field="date" headerText="Date" width="100" />
           <ColumnDirective
@@ -41,8 +41,8 @@ const TransactionGrid = ({ transactionData }) => {
             headerText="Description"
             width="100"
           />
-          <ColumnDirective field="id" headerText="ID" width="100" />
-          <ColumnDirective field="user_id" headerText="User ID" width="100" />
+          {/* <ColumnDirective field="id" headerText="ID" width="100" />
+          <ColumnDirective field="user_id" headerText="User ID" width="100" /> */}
         </ColumnsDirective>
         <Inject
           services={[
