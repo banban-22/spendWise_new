67f2118c8808dfd231e39ec65f842003f031bdc7
AyakaB <a1ya1k2a9@gmail.com>
2023-08-08 20:47:10 -0700

    fixed transaction page

--- client/src/pages/TransactionPage.jsx
+++ client/src/pages/TransactionPage.jsx
@@ -1,69 +1,100 @@
 import React, { useEffect, useState } from 'react';
 import TransactionCreate from '../components/TransactionCreate';
 import TransactionGrid from '../components/TransactionGrid';
-import { Transaction, Category } from '../requests';
+import { Transaction, Category, User } from '../requests';
 
 const TransactionPage = () => {
   const [transactionData, setTransactionData] = useState([]);
   const [categories, setCategories] = useState([]);
-  const [errors, setErrors] = useState([]);
+  const [user, setUser] = useState(null);
   const [isLoading, setIsLoading] = useState(true);
+  const [hasError, setHasError] = useState(false);
 
   useEffect(() => {
-    Transaction.index()
-      .then((data) => {
-        setTransactionData(data);
+    getCurrentUser();
+    fetchTransactions();
+    fetchCategories();
+  }, []);
+
+  const getCurrentUser = () => {
+    User.current()
+      .then((userData) => {
+        if (userData?.id) {
+          setUser(userData);
+        }
       })
       .catch((err) => {
-        setErrors(err);
+        console.log(err);
+        setHasError(true);
       });
+  };
+
+  useEffect(() => {
+    getCurrentUser();
+    fetchCategories();
   }, []);
 
   useEffect(() => {
+    if (user?.id) {
+      fetchTransactions();
+    }
+  }, [user]);
+
+  const fetchTransactions = () => {
+    if (user?.id) {
+      Transaction.index()
+        .then((data) => {
+          const userTransactions = data.filter(
+            (transaction) => transaction.user_id === user.id
+          );
+          setTransactionData(userTransactions);
+        })
+        .catch((err) => {
+          setHasError(true);
+        })
+        .finally(() => {
+          setIsLoading(false);
+        });
+    }
+  };
+
+  const fetchCategories = () => {
     Category.index()
       .then((data) => {
         setCategories(data);
       })
       .catch((err) => {
-        setErrors(err);
+        setHasError(true);
       });
-  }, []);
+  };
 
   const updatedDataInDatabase = async (updatedData) => {
     try {
       const response = await Transaction.update(updatedData.id, updatedData);
       console.log('updatedData', response);
+      fetchTransactions();
     } catch (error) {
       console.error(error);
     }
   };
-
-  // const updatedDataInDatabase = async (updatedData) => {
-  //   try {
-  //     const response = await Transaction.update(updatedData.id, updatedData);
-  //     console.log('updatedData', response);
-
-  //     const updatedIndex = transactionData.findIndex(
-  //       (transaction) => transaction.id === updatedData.id
-  //     );
-  //     if (updatedIndex !== -1) {
-  //       const updatedTransactionData = [...transactionData];
-  //       updatedTransactionData[updatedIndex] = response.transaction;
-  //       setTransactionData(updatedTransactionData);
-  //     }
-  //   } catch (error) {
-  //     console.error(error);
-  //   }
-  // };
 
   const deleteDataInDatabase = async (deleteData) => {
     try {
       const response = await Transaction.destroy(deleteData.id);
       console.log('deleteData', response);
+      fetchTransactions(); // Refresh transactions after deletion
     } catch (error) {
       console.error(error);
     }
   };
+
+  if (isLoading) {
+    return <div>Loading...</div>; // Show loading indicator
+  }
+
+  if (hasError) {
+    return <div>Error fetching data</div>; // Show error message
+  }
 
   return (
     <div className="m-3">
