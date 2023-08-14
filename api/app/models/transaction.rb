f91c90bbf7e850ba3ad3d156a46a0eb190fb00d5
AyakaB <a1ya1k2a9@gmail.com>
2023-08-14 16:05:13 -0700

    remove currenct presence

--- api/app/models/transaction.rb
+++ api/app/models/transaction.rb
@@ -4,7 +4,6 @@ class Transaction < ApplicationRecord
 
     validates :amount, presence: true, numericality: { greater_than: 0 }
     validates :date, presence: true
-    validates :currency, presence: true
     validates :transaction_type, presence: true
     validates :transaction_type, inclusion: { in: ['expenditure', 'income']  }
 end
